5b51c5bd9f3f999cd7870afa0dd950a163434941
hildxd <hildxd@outlook.com>
2024-11-20 15:48:04 +0800

    chore: init project

---
+++ chat-server/src/config.rs
@@ -0,0 +1,33 @@
+use std::{env, fs::File, path::Path};
+
+use anyhow::{bail, Result};
+use serde::{Deserialize, Serialize};
+
+#[derive(Debug, Serialize, Deserialize)]
+pub struct AppConfig {
+    pub server: ServerConfig,
+}
+
+#[derive(Debug, Serialize, Deserialize)]
+pub struct ServerConfig {
+    pub port: u16,
+}
+
+impl AppConfig {
+    pub fn load() -> Result<Self> {
+        let project_root = env!("CARGO_MANIFEST_DIR");
+        let project_config_path = Path::new(&project_root).join("app.yml");
+
+        let ret = match (
+            File::open(&project_config_path),
+            File::open("~/.config/crablink/app.yml"),
+            env::var("CHAT_CONFIG"),
+        ) {
+            (Ok(file), _, _) => serde_yaml::from_reader(file),
+            (_, Ok(file), _) => serde_yaml::from_reader(file),
+            (_, _, Ok(path)) => serde_yaml::from_reader(File::open(path)?),
+            _ => bail!("no config found"),
+        };
+        Ok(ret?)
+    }
+}
---
+++ chat-server/src/main.rs
@@ -0,0 +1,14 @@
+use anyhow::Result;
+use chat_server::AppConfig;
+use tracing::{info, level_filters::LevelFilter};
+use tracing_subscriber::{fmt::Layer, layer::SubscriberExt, util::SubscriberInitExt, Layer as _};
+
+#[tokio::main]
+async fn main() -> Result<()> {
+    let layer = Layer::new().with_filter(LevelFilter::INFO);
+    tracing_subscriber::registry().with(layer).init();
+
+    let config = AppConfig::load()?;
+
+    Ok(())
+}
